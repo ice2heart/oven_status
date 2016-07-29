6ff823671f5cd74509b6a676b2b3f1eee073d9bb
A.G. Yakupov <ag.yakupov@tensor.ru>
2016-07-29 15:46:02 +0500

    blessed

--- app.js
+++ app.js
@@ -1,5 +1,31 @@
-var mqtt    = require('mqtt');
-var client  = mqtt.connect('mqtt://localhost');
+//https://github.com/knolleary/pubsubclient/blob/master/examples/mqtt_esp8266/mqtt_esp8266.ino -- esp
+
+const mqtt    = require('mqtt');
+const client  = mqtt.connect('mqtt://localhost');
+const blessed = require('blessed');
+// Create a screen object.
+const screen = blessed.screen({
+  smartCSR: true
+});
+
+const box = blessed.log({
+  top: 'center',
+  bottom: 1,
+  width: '80%',
+  height: '100%',
+  content: 'Hello {bold}world{/bold}!',
+  tags: true,
+  border: {
+    type: 'line'
+  }
+});
+screen.append(box);
+
+
+// Quit on Escape, q, or Control-C.
+screen.key(['escape', 'q', 'C-c'], function(ch, key) {
+  return process.exit(0);
+});
 
 client.on('connect', function () {
   client.subscribe('oven');
